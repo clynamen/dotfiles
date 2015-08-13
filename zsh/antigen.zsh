29540fc6426301ff6db5b64e81d17088caef0fe2
clynamen <clynamen@gmail.com>
2015-08-13 12:34:57 +0200

    add new zsh packages

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -2,7 +2,7 @@
 source /usr/share/zsh/scripts/antigen/antigen.zsh
 
 #Load the oh-my-zsh's library.
-antigen use oh-my-zsh
+#antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
 #antigen bundle git
@@ -12,6 +12,7 @@ antigen use oh-my-zsh
 #antigen bundle command-not-found
 
 # Syntax highlighting bundle.
+antigen bundle robbyrussell/oh-my-zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Load the theme.
